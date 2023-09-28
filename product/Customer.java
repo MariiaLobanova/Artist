21280d5fe7b638cdfe80785114d94a7fffbfd5a4
Lobanova_Mariia <lobanovmariia@gmail.com>
2023-09-28 15:25:22 +0200

    added new class Customer

---
+++ product/Customer.java
@@ -0,0 +1,20 @@
+package product;
+
+public class Customer {
+
+    private String nameProduct;
+
+    public Customer(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public void addToCart(String item) {
+        System.out.println("Added " + item + " to " + nameProduct + "'s cart.");
+    }
+
+    public static void main(String[] args) {
+
+        Customer customer1 = new Customer("Elena");
+        customer1.addToCart("Item1");
+    }
+}
