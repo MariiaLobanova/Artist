58e8f8551ae91747348ce5dbc0a62c2ea8aaeaaa
Elena Bezrodnova <elenabezrodnova@Elenas-Air.fritz.box>
2023-09-28 15:43:34 +0200

    update product class

--- product/Product.java
+++ product/Product.java
@@ -3,13 +3,28 @@ package product;
 public class Product {
     String name;
     int price;
-    String stock;
+    int stock;
+
+    public Product(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
 
     public String getDetails() {
-        return "Stock";
+        String result = String.format("Name: %s\nPrice: %s\nStock: %s\n", name, price, stock);
+        return result;
     }
-    public void updateStock(String stock) {
-this.stock = stock;
-    }
+    public void updateStock(int count) {
+        if (count < 0) {
+            if (stock + count < 0) {
+                System.out.println("Sorry. We don't have this amount in our stock");
+            }
+        }
+        else {
+            this.stock += count;
+            System.out.println("Stock" + stock);
 
+        }
+    }
 }
